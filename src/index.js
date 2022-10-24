aed4beecc9698a2395d35e540251012b06c0f336
Ray Blair <ray_blair@epam.com>
2022-10-24 17:10:15 +0100

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Component from './Component';
-import CreateElement from './CreateElement';
-import Functional from './Functional';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CreateElement />
-    <Component />
-    <Functional />
+    <App />
   </React.StrictMode>
 );
 
